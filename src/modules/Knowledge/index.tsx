3ec2ccc3082d16f474d5de98b5f8d33600098d75
Nikolas Santos <nikolas@buildbox.com.br>
2020-12-27 17:34:51 -0300

    new fix

--- src/modules/Knowledge/index.tsx
+++ src/modules/Knowledge/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { SecondCardBox } from '../../components/secondCardBox'
 import { CardBoxContainer, KnowledgeWrapper } from './styles'
 
-export default function Knowledge() {
+export function Knowledge() {
   return (
     <KnowledgeWrapper>
       <h1>Conhecimentos</h1>
